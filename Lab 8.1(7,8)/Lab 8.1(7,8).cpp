594968079e3508402bd014f25c47ef4e5774b400
Rumpl3slaw <wowpanda13@ukr.net>
2020-11-28 17:23:09 +0200

    Lab 8.1(7,8)

---
+++ Lab 8.1(7,8)/Lab 8.1(7,8).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Print(const string A)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = A.find("while", pos, 2)) < A.size())
+	{
+		k++;
+		pos += 2;
+	}
+	return k;
+}
+string Change(string& A)
+{
+	size_t pos = 0;
+	while ((pos = A.find("while", pos, 2)) < A.size())
+		A.replace(pos, 2, "*");
+	return A;
+}
+int main()
+{
+	string A;
+	
+	cout << "VVedit` stroku";
+	getline(cin, A);
+
+	cout << "Chyslo" << Print(A) << endl;
+	cout << "Modified string (param):" << Change(A) << endl;
+	return 0;
+}
